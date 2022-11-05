9d0b656a58f753ba654108e74e969e697e96fd4e
Dalyclnl <dalyarmijoa@yahoo.com>
2022-11-05 18:10:00 +0000

    2th commit

---
+++ src/js/component/TrafficL.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { container } from "webpack";
+
+const [ color, setColor] = useState("black");
+
+const color = ["red", "green", "yellow"];
+
+    function ChangeColor() {
+
+    document.getElementById("red-ligth").style.backgroundColor=('black');
+    document.getElementById("yellow-ligth").style.backgroundColor=('black');   
+    document.getElementById("green-ligth").style.backgroundColor=('black');    
+
+    red.addEventListener('click', function(){setColor()}, false);
+}
+
+
+
+	
+export default TrafficL
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,5 @@
 import React ,{useState}from "react";
+import TrafficL from "./TrafficL";
 
 
 
@@ -6,18 +7,8 @@ import React ,{useState}from "react";
 	
 const Home =() => {
 
-function ligthOn () {
-	const color = ["red","yellow","green"];
-	const  [litpos,setLitpos] =useState(null);
 
-	<div className="container">
-		   <div id="red-ligth" style={{backgroundColor:litpos ===0 ? "red": "white" }} 
-		   oneClick={() => 'setLitPos'(litpos === 0 ? -1 :0)} /> 
-		   <div id="yellow-ligth" style={{backgroundColor:litpos === 1 ? "yellow": "white" }} 
-		   oneClick={() => "setLitPos"(litpos === 1 ? -1 :1)} />
-		   <div id="gree-ligth" style={{backgroundColor:litpos === 2 ? "green": "white" }} 
-		   oneClick={() => setLitPos(litpos === 2 ? -1 :2)} />             
-   </div>
+	<di><TrafficL/> </di>
+
 };
-}
 export default Home;
